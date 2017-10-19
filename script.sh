aec449f0200069137fc9ab13a006708096432c0e
Alvin <defalcator@gmail.com>
2017-10-18 22:56:38 -0400

    Update and rename README.md to script.sh

--- script.sh
+++ script.sh
@@ -14,13 +14,4 @@ for j in `cat "match/$i-listofcipher.txt"`; do cat openssl-rfc.mapping.html | gr
 done
 #cat "$i" ; echo '  ' ;  cat "cross/$i matching.txt"
 
-############################# for example ############################
-AES128-GCM-SHA256 (TLS_RSA_WITH_AES_128_GCM_SHA256)
-AES128-SHA (TLS_RSA_WITH_AES_128_CBC_SHA)
-AES128-SHA256 (TLS_RSA_WITH_AES_128_CBC_SHA256)
-AES256-GCM-SHA384 (TLS_RSA_WITH_AES_256_GCM_SHA384)
-AES256-SHA (TLS_RSA_WITH_AES_256_CBC_SHA)
-AES256-SHA256 (TLS_RSA_WITH_AES_256_CBC_SHA256)
-CAMELLIA128-SHA (TLS_RSA_WITH_CAMELLIA_128_CBC_SHA)
-CAMELLIA256-SHA (TLS_RSA_WITH_CAMELLIA_256_CBC_SHA)
 
